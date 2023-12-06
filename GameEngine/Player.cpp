f1918225cf2f899549790fbe80acc945bc61483c
Tor Nordmark <30878484+Play4leftovers@users.noreply.github.com>
2023-12-06 15:57:13 +0100

    Quick and Simple player script

---
+++ GameEngine/Player.cpp
@@ -0,0 +1,20 @@
+﻿#include "Player.h"
+
+void Player::Move(float input)
+{
+    MovementDirection.x = input * movementSpeed;
+}
+
+void Player::Jump()
+{
+    if(isGrounded)
+    {
+        MovementDirection.y = jumpSpeed;
+        SetGrounded(false);
+    }
+}
+
+void Player::SetGrounded(bool grounding)
+{
+    isGrounded = grounding;
+}
---
+++ GameEngine/Player.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "Vector2.h"
+
+class Player
+{
+public:
+    Vector2 MovementDirection;
+    
+private:
+    float jumpSpeed;
+    float movementSpeed;
+
+    bool isGrounded;
+    
+public:
+    void Move(float input);
+    void Jump();
+    void SetGrounded(bool grounding);
+};
